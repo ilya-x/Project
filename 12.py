43b7b4fcb5016bec2b366b616ad50a308e486288
ilya-x <ilya-x@mail.com>
2019-04-15 18:03:27 +0300

    12

---
+++ 12.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:59:51) [MSC v.1914 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> int(input("Ведите целое число, по моду не превосходящее 1000 ")
+Next = Num + 1
+Previous = Num - 1
+Print ("The next number for the number " Num is Next)
+Print ("The Previous number for the number " Num is Previous )
+
